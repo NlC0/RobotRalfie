cc277b4e4a8a7ea0b3e35ddb64b4c686f4fe0a4c
RobotRalfie <59607351+RobotRalfie@users.noreply.github.com>
2020-01-07 13:09:34 +0100

    Add files via upload

---
+++ ROBOT_PRJ1.ino
@@ -0,0 +1,26 @@
+int mySens[4] = {3, 5, 6, 9};
+
+#define  pwmA 3 //left side
+#define pwmB 11 // right side
+
+#define breakeA 9
+#define breakeB 8
+#define dirA 12
+#define dirB 13
+
+
+void setup() {
+
+  pinMode(9, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(13, OUTPUT);
+
+
+}
+
+void loop() {
+
+
+
+}
